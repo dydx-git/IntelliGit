c9396941330e3535d0b72a7d908b180d8ae2348c
Taha <muhammad.taha@astera.com>
2024-06-13 17:35:38 +0500

    added command line parser

---
+++ IntelliGit/Commands/IntelliGitArguments.cs
@@ -0,0 +1,9 @@
+﻿using LibGit2Sharp;
+
+namespace IntelliGit;
+
+public record IntelliGitArguments
+{
+    public bool Staged { get; set; }
+    public int NumberOfEstimatedCommits { get; set; }
+}
--- IntelliGit/Program.cs
+++ IntelliGit/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace IntelliGit;
+﻿using Fclp;
+
+namespace IntelliGit;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var parser = new FluentCommandLineParser<IntelliGitArguments>();
+        parser.Setup(arg => arg.Staged)
+            .As('s', "staged")
+            .Required();
+        parser.Setup(arg => arg.NumberOfEstimatedCommits)
+            .As('c', "num")
+            .Required();
+
+        var programArgs = parser.Parse(args);
     }
 }
